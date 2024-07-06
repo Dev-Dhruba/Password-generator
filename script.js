67aa24dec5e2a2eb421eb996db46d5fcc86a1e71
Dhruba <dhrubabhattacharyya13@gmail.com>
2024-07-06 19:09:51 +0530

    Gernrator working

--- script.js
+++ script.js
@@ -6,15 +6,19 @@ const numbers = '1234567890';
 const symbols = '!@#$%&';
 const allChars = upperCase + lowerCase + numbers + symbols;
 
-let password = '';
-let passwordLength = 16;
 
-password += upperCase[Math.floor(Math.random() * upperCase.length)];
-password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-password += numbers[Math.floor(Math.random() * numbers.length)];
-password += symbols[Math.floor(Math.random() * symbols.length)];
+function passwordGive(){
+    let password = '';
+    let passwordLength = 16;
 
-while(passwordLength > password.length){
-    password += allChars[Math.floor(Math.random() * allChars.length)];
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += symbols[Math.floor(Math.random() * symbols.length)];
+
+    while(passwordLength > password.length){
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    inputField.value = password;
 }
 
